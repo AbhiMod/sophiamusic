a9feb6a1be9277a74139ab0e31b1b12e5382ce5a
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-17 04:55:40 +0530

    Update logger.py

--- am/utils/logger.py
+++ am/utils/logger.py
@@ -11,23 +11,14 @@ async def play_logs(message, streamtype):
             chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
         logger_text = f"""
 ╔════❰𝐏𝐋𝐀𝐘𝐈𝐍𝐆❱═══❍⊱❁۪۪
-
 ◈ 𝐂𝐡𝐚𝐭 ➪ **{message.chat.title}**
-
 ◈ 𝐂𝐡𝐚𝐭 𝐈𝐝 ➪ `{message.chat.id}`
-
 ◈ 𝐔𝐬𝐞𝐫 ➪ **{message.from_user.mention}**
-
 ◈ 𝐔𝐬𝐞𝐫𝐧𝐚𝐦𝐞 ➪ **@{message.from_user.username}**
-
 ◈ 𝐈𝐝 ➪ `{message.from_user.id}`
-
 ◈ 𝐂𝐡𝐚𝐭 𝐋𝐢𝐧𝐤 ➪ **{chatusername}**
-
 ◈ 𝐒𝐞𝐚𝐫𝐜𝐡𝐞𝐝 ➪ `{message.text}`
-
 ◈ 𝐁𝐲 ➪ **{streamtype} ▄ █ ▄ █ ▄**
-
 ╚═══❰ #𝐍𝐞𝐰𝐒𝐨𝐧𝐠 ❱══❍⊱❁۪۪"""
         if message.chat.id != LOG_GROUP_ID:
             try:
